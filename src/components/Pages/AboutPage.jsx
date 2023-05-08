3cdca47dcfccf06981437784c20dd96a3db7f90c
unknown <priyanka@metalok.io>
2023-05-08 16:01:25 +0530

    content of initial was replaced

---
+++ src/components/Pages/AboutPage.jsx
@@ -0,0 +1,171 @@
+import React, { useEffect } from 'react';
+import { pageTitle } from '../../helper';
+import Cta from '../Cta';
+import FunFact from '../FunFact';
+import PageHeading from '../PageHeading';
+import Div from '../Div';
+import SectionHeading from '../SectionHeading';
+import TeamSlider from '../Slider/TeamSlider';
+import Spacing from '../Spacing';
+
+export default function AboutPage() {
+  pageTitle('About');
+
+  const funfaceData = [
+    {
+      title: 'Global Happy Clients',
+      factNumber: '40K',
+    },
+    {
+      title: 'Project Completed',
+      factNumber: '50K',
+    },
+    {
+      title: 'Team Members',
+      factNumber: '245',
+    },
+    {
+      title: 'Digital products',
+      factNumber: '550',
+    },
+  ];
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <>
+      {/* Start Page Heading Section */}
+      <PageHeading
+        title="About Us"
+        bgSrc="images/about_hero_bg.jpeg"
+        pageLinkText="About Us"
+      />
+      {/* End Page Heading Section */}
+
+      {/* Start About Section */}
+      <Spacing lg="150" md="80" />
+      <Div className="container">
+        <Div className="row">
+          <Div className="col-xl-5 col-lg-7">
+            <SectionHeading
+              title="A one-stop destination to all your Virtual Needs "
+              subtitle="About Our Agency"
+            >
+              <Spacing lg="30" md="20" />
+              <p className="cs-m0">
+              We are an agency creating, building and thriving digital transformation, experiential platforms, 
+              change management, and data management through
+               human-centric futuristic technologies of AI, AR, VR, XR, Mixed Reality, and Blockchain. 
+              </p>
+              <Spacing lg="30" md="30" />
+              <Div className="cs-separator cs-accent_bg"></Div>
+              <Spacing lg="25" md="40" />
+            </SectionHeading>
+          </Div>
+          <Div className="col-lg-5 offset-xl-2">
+            <img
+              src="/images/about_img_1.jpeg"
+              alt="About"
+              className="w-100 cs-radius_15"
+            />
+            <Spacing lg="25" md="25" />
+          </Div>
+          <Div className="col-lg-7">
+            <img
+              src="/images/about_img_2.jpeg"
+              alt="About"
+              className="w-100 cs-radius_15"
+            />
+            <Spacing lg="25" md="25" />
+          </Div>
+          <Div className="col-lg-5">
+            <img
+              src="/images/about_img_3.jpeg"
+              alt="About"
+              className="w-100 cs-radius_15"
+            />
+            <Spacing lg="25" md="25" />
+          </Div>
+        </Div>
+      </Div>
+      <Spacing lg="75" md="55" />
+      {/* End About Section */}
+
+      {/* Start Fun Fact Section */}
+      <Div className="container">
+        <FunFact
+          title="Our Core Values"
+          subtitle="Sed ut perspiciatis unde omnis iste natus error voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis."
+          data={funfaceData}
+        />
+      </Div>
+      {/* End Fun Fact Section */}
+
+      {/* Start Why Choose Section */}
+      <Spacing lg="100" md="80" />
+      <Div className="container">
+        <Div className="row">
+          <Div className="col-xl-5 col-lg-6">
+            <Div className="cs-image_layer cs-style1">
+              <Div className="cs-image_layer_in">
+                <img
+                  src="/images/about_img_4.jpeg"
+                  alt="About"
+                  className="w-100 cs-radius_15"
+                />
+              </Div>
+            </Div>
+            <Spacing lg="0" md="40" />
+          </Div>
+          <Div className="col-xl-5 offset-xl-1 col-lg-6">
+            <SectionHeading
+              title="Highly experienced pepole with us"
+              subtitle="Why Choose Us"
+            >
+              <Spacing lg="30" md="20" />
+              <p className="cs-m0">
+              Metalok offers its services in the emerging Web 3 space. We offer Platform-as-a-service (PaaS),
+               Software-as-a-service (SaaS), Lifestyle-as-a-service (LSaaS), 
+              Experience-as-a-service (EaaS), and Learning-as-a-service (LaaS).
+              </p>
+              <Spacing lg="15" md="15" />
+              <p className="cs-m0">
+              These categories include departmentalized services and consultation for businesses
+               under Architecture, Design and Technology sections independently and as end-to-end solutions
+              </p>
+              <Spacing lg="30" md="30" />
+              <Div className="cs-separator cs-accent_bg"></Div>
+              <Spacing lg="25" md="0" />
+            </SectionHeading>
+          </Div>
+        </Div>
+      </Div>
+      {/* End Why Choose Section */}
+
+      {/* Start Team Section */}
+      <Spacing lg="145" md="80" />
+      <Div className="container">
+        <SectionHeading
+          title="Awesome team <br/>members"
+          subtitle="Our Team"
+          variant="cs-style1"
+        />
+        <Spacing lg="85" md="45" />
+        <TeamSlider />
+      </Div>
+      {/* End Team Section */}
+
+      {/* Start CTA Section */}
+      <Spacing lg="150" md="80" />
+      <Div className="container">
+        <Cta
+          title="Let’s disscuse make <br />something <i>cool</i> together"
+          btnText="Apply For Meeting"
+          btnLink="/contact"
+          bgSrc="/images/cta_bg.jpeg"
+        />
+      </Div>
+      {/* End CTA Section */}
+    </>
+  );
+}
---
+++ src/components/Slider/TeamSlider.jsx
@@ -0,0 +1,162 @@
+import { Icon } from '@iconify/react';
+import React from 'react';
+import Slider from 'react-slick';
+import Div from '../Div';
+import Team from '../Team';
+
+export default function TeamSlider() {
+  /** Team Member Data **/
+  const teamData = [
+    {
+      memberImage: '/images/member_1.jpeg',
+      memberName: 'Aanchal Goel',
+      memberDesignation: 'chief financial officerr',
+      memberSocial: {
+        linkedin: '/',
+        twitter: '/',
+        youtube: '/',
+        facebook: '/',
+      },
+    },
+    {
+      memberImage: '/images/member_2.jpeg',
+      memberName: 'Mohit Goel',
+      memberDesignation: 'chief executive officer',
+      memberSocial: {
+        linkedin: '/',
+        twitter: '/',
+        youtube: '/',
+        facebook: '/',
+      },
+    },
+    {
+      memberImage: '/images/member_3.jpeg',
+      memberName: 'Smitha PM ',
+      memberDesignation: 'chief Human resource officer',
+      memberSocial: {
+        linkedin: '/',
+        twitter: '/',
+        youtube: '/',
+        facebook: '/',
+      },
+    },
+    {
+      memberImage: '/images/member_4.jpeg',
+      memberName: 'Parth Saradhi Reddy',
+      memberDesignation: 'chief Product officer',
+      memberSocial: {
+        linkedin: '/',
+        twitter: '/',
+        youtube: '/',
+        facebook: '/',
+      },
+    },
+    {
+      memberImage: '/images/member_4.jpeg',
+      memberName: 'Him Agarwal ',
+      memberDesignation: 'chief technical officer',
+      memberSocial: {
+        linkedin: '/',
+        twitter: '/',
+        youtube: '/',
+        facebook: '/',
+      },
+    },
+    {
+      memberImage: '/images/member_4.jpeg',
+      memberName: 'Pourush Dhingra  ',
+      memberDesignation: 'chief design officer',
+      memberSocial: {
+        linkedin: '/',
+        twitter: '/',
+        youtube: '/',
+        facebook: '/',
+      },
+    },
+    {
+      memberImage: '/images/member_4.jpeg',
+      memberName: 'Raghuram ',
+      memberDesignation: 'chief architect officer',
+      memberSocial: {
+        linkedin: '/',
+        twitter: '/',
+        youtube: '/',
+        facebook: '/',
+      },
+    },
+  ];
+  /** Slider Settings **/
+  const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
+    <div
+      {...props}
+      className={
+        'slick-prev slick-arrow' + (currentSlide === 0 ? ' slick-disabled' : '')
+      }
+      aria-hidden="true"
+      aria-disabled={currentSlide === 0 ? true : false}
+    >
+      <Icon icon="bi:arrow-left" />
+    </div>
+  );
+  const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
+    <div
+      {...props}
+      className={
+        'slick-next slick-arrow' +
+        (currentSlide === slideCount - 1 ? ' slick-disabled' : '')
+      }
+      aria-hidden="true"
+      aria-disabled={currentSlide === slideCount - 1 ? true : false}
+    >
+      <Icon icon="bi:arrow-right" />
+    </div>
+  );
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: <SlickArrowLeft />,
+    nextArrow: <SlickArrowRight />,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          dots: true,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  };
+
+  return (
+    <Slider {...settings} className="cs-gap-24 cs-arrow_style2">
+      {teamData.map((item, index) => (
+        <Div key={index}>
+          <Team
+            memberImage={item.memberImage}
+            memberName={item.memberName}
+            memberDesignation={item.memberDesignation}
+            memberSocial={item.memberSocial}
+          />
+        </Div>
+      ))}
+    </Slider>
+  );
+}
